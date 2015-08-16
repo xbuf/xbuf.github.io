e5ca5363823137a65fa8f16b7bb85a2cf74586b3
David Bernard <d.bernard1@ssi.samsung.com>
2015-08-16 11:57:21 +0200

    theme: setup site0 based on foundation-5.5.2

---
+++ themes/site0/static/js/app.js
@@ -0,0 +1,15 @@
+/* http://zurb.com/building-blocks/off-canvas-sidebar-component */
+function sideNav() {
+  if ($(window).width() < 769) {
+    $('.off-canvas-wrap').removeClass('move-right');
+    $('.left-off-canvas-toggle').show();
+  } else {
+    $('.off-canvas-wrap').addClass('move-right');
+    $('.left-off-canvas-toggle').hide();
+  }  
+}
+
+$(window).resize(function() {
+  sideNav();
+});
+/* -------------------------------------- */
